4abee5fbbc6795a6772f704566b62bc6388eebf7
rogueeeee <61908580+rogueeeee@users.noreply.github.com>
2021-03-04 03:04:30 +0800

    fixed pattern scanner comparing against signed and unsigned types causing match failure

--- utils/pattern_scanner.cpp
+++ utils/pattern_scanner.cpp
@@ -1,18 +1,18 @@
 #include "pattern_scanner.h"
 #include <cstring>
 
-std::uint8_t *ida_pattern_scan(void *base, std::size_t size, const char *pattern, const char *mask)
+std::uint8_t *pattern_scan(void *start, std::size_t size, const char *pattern, const char *mask)
 {
-	std::uint8_t       *current        = reinterpret_cast<std::uint8_t *>(base);
-	const std::uint8_t *end            = current + size;
-	const std::size_t   pattern_length = strlen(mask) + 1;
+	char               *current        = reinterpret_cast<char *>(start);
+	const void         *end            = current + size;
+	const std::size_t  pattern_length = strlen(mask) + 1;
 
 	do
 	{
-		for (std::size_t idx = 0; idx < pattern_length; idx++)
+		for (std::size_t idx = 0; idx <= pattern_length; idx++)
 		{
 			if (mask[idx] == '\0')
-				return current;
+				return reinterpret_cast<std::uint8_t *>(current);
 
 			if (mask[idx] == '?')
 				continue;
--- utils/pattern_scanner.h
+++ utils/pattern_scanner.h
@@ -3,4 +3,4 @@
 #include <cstdint>
 #include <cstddef>
 
-std::uint8_t *ida_pattern_scan(void *base, std::size_t size, const char *pattern, const char *mask);
+std::uint8_t *pattern_scan(void *start, std::size_t size, const char *pattern, const char *mask);
