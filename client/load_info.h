40cf7e276abc2c8af3496f1df0a5ba846d27becb
rogueeeee <61908580+rogueeeee@users.noreply.github.com>
2021-03-14 00:53:33 +0800

    fix reloc, setup loader_info_t, setup and write x86 shellcode to execute entry point

--- client/load_info.h
+++ client/load_info.h
@@ -8,7 +8,7 @@ struct load_info_t
 	decltype(LoadLibraryA)   *import_LoadLibraryA   = nullptr;
 	decltype(GetProcAddress) *import_GetProcAddress = nullptr;
 	std::uint8_t			 *dll_base              = nullptr;
-	std::ptrdiff_t           *rebase_delta          = nullptr;
+	std::ptrdiff_t            rebase_delta          = NULL;
 	PIMAGE_BASE_RELOCATION    image_base_reloc      = nullptr;
 	PIMAGE_IMPORT_DESCRIPTOR  image_import_desc     = nullptr;
 	BOOL(APIENTRY *DllMain)(HMODULE, DWORD, LPVOID) = nullptr;
--- injector/injector.cpp
+++ injector/injector.cpp
@@ -7,10 +7,9 @@
 #include <cstdint>
 #include <filesystem>
 #include <fstream>
-
+#include <client/load_info.h>
 #include "inj_utils.h"
 #include "injector_return_code.h"
-
 #include "binaries/client_binary.h"
 static_assert(sizeof(client_bin) >= sizeof(IMAGE_DOS_HEADER), "Injector binary headers was not implemented. Refer: https://github.com/rogueeeee/patchii2#Building-Injector");
 
@@ -32,7 +31,6 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	}
 	catch (std::exception ex)
 	{
-		MessageBoxW(nullptr, (std::wstring(L"patchii injector\n\nPROC_ID_STOI_EXCEPTION\n\n") + lpCmdLine).c_str(), L"", MB_ICONERROR);
 		return_as_code(PROC_ID_STOI_EXCEPTION);
 	}
 	
@@ -45,14 +43,70 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	
 	#if defined( PATCHII_LOADAS_MANUALMAPPED )
 	{
-		std::uint8_t *alloc_address = reinterpret_cast<std::uint8_t *>(VirtualAllocEx(proc_open, nullptr, pe_get_ntheaderptr(client_bin)->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE));
+		PIMAGE_NT_HEADERS nt_header = pe_get_ntheaderptr(client_bin);
+		
+		#ifdef _M_IX86
+		constexpr std::ptrdiff_t sc_offset_entrypoint = 0x6;
+		constexpr std::ptrdiff_t sc_offset_loadinfo   = 0xB;
+
+		std::uint8_t shellcode[] =
+		{
+			0x55,						   // push   ebp
+			0x89, 0xE5,					   // mov    ebp, esp
+			0x51,						   // push   ecx
+			0x52,						   // push   edx
+			0xB9, 0x00, 0x00, 0x00, 0x00,  // mov    ecx, dll_alloc + nt_header->OptionalHeader.AddressOfEntryPoint
+			0xBA, 0x00, 0x00, 0x00, 0x00,  // mov    edx, load_info_alloc
+			0x52,						   // push   edx
+			0x6A, 0x01,					   // push   0x1
+			0x6A, 0x00,					   // push   0x0
+			0xFF, 0xD1,					   // call   ecx
+			0x31, 0xC0,					   // xor    eax, eax
+			0x5A,						   // pop    edx
+			0x59,						   // pop    ecx
+			0x5D,						   // pop    ebp
+			0xC2, 0x00, 0x00			   // ret    0x0
+		};
+		#elif _M_X64
+		std::uint8_t shellcode[] =
+		{
+			0x00
+		};
+		#endif
+
+		std::uint8_t *alloc_address = reinterpret_cast<std::uint8_t *>(VirtualAllocEx(proc_open, nullptr, sizeof(shellcode) + sizeof(load_info_t) + nt_header->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE));
 		if (!alloc_address)
 			return_as_code(FAILED_TO_ALLOCATE);
-
-		for (std::size_t idx_sect = 0; idx_sect < pe_get_ntheaderptr(client_bin)->FileHeader.NumberOfSections; idx_sect++)
+		
+		std::uint8_t          *shellcode_alloc = alloc_address;
+		load_info_t           *load_info_alloc = reinterpret_cast<load_info_t *>(alloc_address + sizeof(shellcode));
+		std::uint8_t          *dll_alloc       = alloc_address + sizeof(shellcode) + sizeof(load_info_t);
+		PIMAGE_SECTION_HEADER  sect_header     = reinterpret_cast<PIMAGE_SECTION_HEADER>(nt_header + 1);
+		for (std::size_t idx = 0; idx < nt_header->FileHeader.NumberOfSections; idx++)
 		{
-
+			if (!WriteProcessMemory(proc_open, dll_alloc + sect_header[idx].VirtualAddress, client_bin + sect_header[idx].PointerToRawData, sect_header[idx].SizeOfRawData, nullptr))
+				return_as_code(FAILED_TO_COPY_SECTION);
 		}
+
+		load_info_t loader_info =
+		{
+			&LoadLibraryA,
+			&GetProcAddress,
+			dll_alloc,
+			reinterpret_cast<std::ptrdiff_t>(dll_alloc - nt_header->OptionalHeader.ImageBase),
+			reinterpret_cast<PIMAGE_BASE_RELOCATION>(dll_alloc + nt_header->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress),
+			reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(dll_alloc + nt_header->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress),
+			reinterpret_cast<decltype(load_info_t::DllMain)>(dll_alloc + nt_header->OptionalHeader.AddressOfEntryPoint)
+		};
+
+		if (!WriteProcessMemory(proc_open, load_info_alloc, &loader_info, sizeof(load_info_t), nullptr))
+			return_as_code(FAILED_TO_WRITE_LOADER_INFO);
+
+		*reinterpret_cast<void **>(shellcode + sc_offset_entrypoint) = loader_info.DllMain;
+		*reinterpret_cast<void **>(shellcode + sc_offset_loadinfo)   = load_info_alloc;
+
+		if (!WriteProcessMemory(proc_open, shellcode_alloc, shellcode, sizeof(shellcode), nullptr))
+			return_as_code(FAILED_TO_WRITE_SHELLCODE);
 	}
 	#elif defined( PATCHII_LOADAS_LOADLIB )
 	{
